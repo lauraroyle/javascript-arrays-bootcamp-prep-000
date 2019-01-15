46f98bcafa4e85e1be808b4bb76fd59ea28d2615
Laura Royle <lauraamie.royle@gmail.com>
2019-01-15 23:11:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,5 +39,5 @@ function destructivelyRemoveElementFromEndOfArray (array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(array.length, - 2);
+  return array.slice(0, - 2);
 }
