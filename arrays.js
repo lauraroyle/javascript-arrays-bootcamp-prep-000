56f205a025b545c662b0e682cb4575a98260b1e4
Laura Royle <lauraamie.royle@gmail.com>
2019-01-15 22:36:34 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray (array, element) {
+  [element, ...array]
+  return array = [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+    array.unshift(element)
+    return array;
+}
+
+function addElementToEndOfArray (array, element) {
+  [...array, element]
+  return array = [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element)
+  return array;
+}
+
+function accessElementInArray (array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  return array.shift();
+}
+
+function destructivelyRemoveElementFromEndOfArray (array) {
+  return array.pop();
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(array.length)
+}
